6fc3b3a3937a96c26db266b31ce7eea0eb0d41c4
kendrit <64579758+kendrit@users.noreply.github.com>
2020-10-24 20:49:31 -0500

    Add files via upload

--- guimain.py
+++ guimain.py
@@ -4,22 +4,40 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import *
 o = 0
+blacklist = []
+engine = 'Google'
+tabs = '5' # these are the default values of all the settings
+keepHistory = 'Yes'
+query = ''
 class window(QMainWindow):
     def __init__(self):
         super(window,self).__init__()
         loadUi("supersearcher.ui", self)
+        blacklist = []
         self.darkMode.clicked.connect(self.darkModeToggle)
         self.pushButton.clicked.connect(self.sendQuery)
         self.radioButtonG.pressed.connect(self.sendEngine)
         self.radioButtonGS.pressed.connect(self.sendEngine)
         self.radioButtonD.pressed.connect(self.sendEngine)
         self.radioButtonB.pressed.connect(self.sendEngine)
-        self.setStyleSheet("background-color: #FFFFFF")
+        self.radioButton1.pressed.connect(self.sendTabs)
+        self.radioButton2.pressed.connect(self.sendTabs)
+        self.radioButton3.pressed.connect(self.sendTabs)
+        self.radioButton4.pressed.connect(self.sendTabs)
+        self.radioButton5.pressed.connect(self.sendTabs)
+        self.radioButtonCom.pressed.connect(self.sendDomains)
+        self.radioButtonEdu.pressed.connect(self.sendDomains)
+        self.radioButtonGov.pressed.connect(self.sendDomains)
+        self.radioButtonOrg.pressed.connect(self.sendDomains)
+        self.radioButtonNet.pressed.connect(self.sendDomains)
+        self.radioYes.pressed.connect(self.keepHistory)
+        self.radioNo.pressed.connect(self.keepHistory)
+        self.setStyleSheet("background-color: #FFFFFF") # this is so the program starts in light mode
         self.label.setStyleSheet("background-color: #FFFFFF")
         self.darkMode.setIcon(QIcon("Darkmode_Inactive.svg"))
         self.settings.setIcon(QIcon("Settings_Inactive.svg"))
         self.queryInput.setStyleSheet("background-color: #dddddd;"
-                                      "font: 32pt Oswald;")
+                                      "font: 32pt Oswald;") # light mode in input box
         self.settings.clicked.connect(self.openSettings)
     def darkModeToggle(self, o):
         o = o + 1
@@ -37,26 +55,33 @@ class window(QMainWindow):
                                           "font: 32pt Oswald;")
         self.label.setStyleSheet(windowbg)
         self.setStyleSheet(windowbg)
-    def sendQuery(self):
-        query = self.queryInput.text()
-        print(query)
-    def sendEngine(self):
+    def keepHistory(self):
+        keepHistory = str(self.sender().text()) # takes the text of the radio button, i.e Yes or No
+        print(keepHistory)
+    def sendDomains(self): # makes/modifies the blacklist of domains
+        print('t')
+        ext = self.sender().text()
+        if ext not in blacklist:
+            blacklist.append(ext)
+        else:
+            blacklist.remove(ext)
+        print(blacklist)
+        return blacklist
+    def sendEngine(self): # grabs the search engine chosen by user live
         engine = self.sender().text()
         print(engine)
-    def openSettings(self):
-        # self.label.setStyleSheet("background-color: transparent")
+    def sendQuery(self): # grabs the text from the input field once the search button is pressed
+        query = self.queryInput.text()
+        print(query)
+    def sendTabs(self): # grabs the preferred number of tabs specified by user, in an integer form
+        tabs = int(self.sender().text())
+        print(tabs)
+    def openSettings(self): # reveals settings options, sends the box thats blocking the settings to the back of the gui
         self.label.lower()
         self.pushButton_2.clicked.connect(self.closeSettings)
-    def closeSettings(self):
+    def closeSettings(self): # hides settings by sending the blocking box up to the front of the gui, and confirms correct execution by printing string after
         self.label.raise_()
-# class settingsMenu(QDialog):
-#     def __init__(self):
-#         super(settingsMenu,self).__init__()
-#         loadUi("settings.ui",self)
-#         self.buttonBox.accepted.connect(self.goBack)
-#     def goBack(self):
-#         widget.addWidget(window())
-#         widget.setCurrentIndex(widget.currentIndex()+1)
+        print('settings confirmed')
 
 app=QApplication(sys.argv)
 mainwindow=window()
